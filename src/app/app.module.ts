85173d911a7bc062e1aaf91107866649b7cfbde7
Ana Patricia García <anapat.garcia@gmail.com>
2019-04-06 18:44:34 +0200

    Creación componente formulario login
    Login Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 //Material theme imports
 import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule, MatIcon } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
     MatButtonModule, 
     MatInputModule,
     MatSelectModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
