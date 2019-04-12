ab0bbacfbd6004e4fe0866a5e3de20e231a62915
Miguel De Arriba <100318627@alumnos.uc3m.es>
2019-04-12 18:27:44 +0200

    Buscador y pantalla de pacientes
    Se ha añadido la pantalla de detalle de pacientes y modificaciones en el buscador.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 import { BuscadorComponent } from './shared/buscador/buscador.component';
+import { PacienteComponent } from './components/paciente/paciente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BuscadorComponent
+    BuscadorComponent,
+    PacienteComponent
   ],
   imports: [
     BrowserModule,
